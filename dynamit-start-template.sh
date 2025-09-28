72a7841251d5a5dffc0e120402a3047831acaf0f
zaki-ananda <88448297+zaki-ananda@users.noreply.github.com>
2025-09-28 15:45:18 +0700

    Added comments for dynamit-start-template.sh

--- dynamit-start-template.sh
+++ dynamit-start-template.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Function: check_command_fail "ErrorMsg"
+# Check whether the previous command fails. If so, print error message "ErrMsg", then exits
 check_command_fail() {
     if [ $? -ne 0 ]; then
         echo "[dynamit-start.sh] Fatal Error: $1"
@@ -7,6 +9,8 @@ check_command_fail() {
     fi
 }
 
+# Function: check_var_empty varname
+# Check whether varname is empty variable. If so, print error message then exits
 check_var_empty() {
     local var_name="$1"
     if [ -z "${!var_name}" ]; then
@@ -14,21 +18,26 @@ check_var_empty() {
         exit 1
     fi
 }
-
+############################################################################################
+# Ensure that DYNAMIT is currently down (not yet started)
 docker compose \
     -f /home/{{ ansible_user_id }}/dynamit/dynamit-run.yaml \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env_dynamit down
 
+# Disable host-wide IPv6 (for better security)
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
+# Initialize all env vars as empty string
 DYNAMIT_HPOT_INTERFACE=""
 DYNAMIT_HPOT_SUBNET=""
 DYNAMIT_SCANHOST_IPADDR=""
 DYNAMIT_MAX_CLUSTER=""
 DYNAMIT_HPOT_HOST_RATIO=""
 DYNAMIT_NEXT_BUILD=""
+
+# Read dotenv files, and check whether all essential env var is nonempty
 set -a && source /home/{{ ansible_user_id }}/dynamit/.env_dynamit && set +a
 check_var_empty "DYNAMIT_SCANHOST_IPADDR"
 check_var_empty "DYNAMIT_HPOT_SUBNET"
@@ -36,9 +45,13 @@ check_var_empty "DYNAMIT_HPOT_INTERFACE"
 check_var_empty "DYNAMIT_MAX_CLUSTER"
 check_var_empty "DYNAMIT_HPOT_HOST_RATIO"
 
+
+# If DYNAMIT_NEXT_BUILD has not been reached, enter this block, where DYNAMIT will use previous honeypot profile configuration. 
 if [[ -n "$DYNAMIT_NEXT_BUILD" && "$(date +%s)" -lt "$DYNAMIT_NEXT_BUILD" ]]; then
     echo "[dynamit-start.sh] Next build time has not been reached. Reusing previous honeynet configuration."
 
+    # If host production interface has valid IP address, remove it, configure firewall to block any traffic to said interface and
+    # to ignore any ARP traffic (to ensure DYNAMIT host doesn't get detected in production network)
     if ip addr show dev "$DYNAMIT_HPOT_INTERFACE" | grep -q 'inet '; then
 	iptables -A INPUT -i ${DYNAMIT_HPOT_INTERFACE} -j DROP 2>/dev/null
 	sysctl -w net.ipv4.conf.${DYNAMIT_HPOT_INTERFACE}.arp_ignore=1
@@ -47,16 +60,22 @@ if [[ -n "$DYNAMIT_NEXT_BUILD" && "$(date +%s)" -lt "$DYNAMIT_NEXT_BUILD" ]]; th
         check_command_fail "[dynamit-start.sh] Fatal Error: Removing IP ${DYNAMIT_SCANHOST_IPADDR} to ${DYNAMIT_HPOT_INTERFACE} failed!"
     fi
 
+    # Run existing compose file to start DYNAMIT
     docker compose \
     -f /home/{{ ansible_user_id }}/dynamit/dynamit-run.yaml \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env_dynamit up
+
+	# Exit when compose failed
     check_command_fail "[dynamit-start.sh] Fatal Error: Composing dynamit-run failed!"
     exit 1
 fi
 
+# If DYNAMIT_NEXT_BUILD has been reached, rebuild honeypot profile configuration
 echo "[dynamit-start.sh] Next build time has been reached. Rebuilding honeynet configuration."
 
+# Assign IP address to production interface, remove firewall config that blocks traffic on said interface,
+# and remove config that ignores ARP traffic
 if ! ip addr show dev "$DYNAMIT_HPOT_INTERFACE" | grep -q 'inet '; then
     iptables -D INPUT -i ${DYNAMIT_HPOT_INTERFACE} -j DROP 2>/dev/null
     sysctl -w net.ipv4.conf.${DYNAMIT_HPOT_INTERFACE}.arp_ignore=0
@@ -65,6 +84,7 @@ if ! ip addr show dev "$DYNAMIT_HPOT_INTERFACE" | grep -q 'inet '; then
     check_command_fail "[dynamit-start.sh] Fatal Error: Assigning IP ${DYNAMIT_SCANHOST_IPADDR} to ${DYNAMIT_HPOT_INTERFACE} failed!"
 fi
 
+# Run honeypot config builder, creating new dynamit-run.yaml file
 docker run --rm \
     -v /home/{{ ansible_user_id }}/dynamit/dynamit-run.yaml:/dynamit-run.yaml:rw \
     -v /home/{{ ansible_user_id }}/dynamit/data/:/data/:rw \
@@ -75,11 +95,14 @@ docker run --rm \
     dynamit-builder:1.0
 check_command_fail "[dynamit-start.sh] Fatal Error: Failure at dynamit-start container!"
 
+# From the newly-created dynamit-run.yaml, pull any images not yet present in local image repo
 docker compose \
     -f /home/{{ ansible_user_id }}/dynamit/dynamit-run.yaml \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env_dynamit pull
 
+# Remove IP address from host production interface, configure firewall to block any traffic to said interface and
+# to ignore any ARP traffic (to ensure DYNAMIT host doesn't get detected in production network)
 iptables -A INPUT -i ${DYNAMIT_HPOT_INTERFACE} -j DROP 2>/dev/null
 sysctl -w net.ipv4.conf.${DYNAMIT_HPOT_INTERFACE}.arp_ignore=1
 sysctl -w net.ipv4.conf.${DYNAMIT_HPOT_INTERFACE}.arp_filter=1
@@ -90,14 +113,18 @@ check_command_fail "[dynamit-start.sh] Fatal Error: Removing IP ${DYNAMIT_SCANHO
 echo "[dynamit-start.sh] Rebuilding honeynet configuration successful."
 echo "[dynamit-start.sh] Next honeynet rebuild will be done at $(date -d 'next week')"
 
+# Update DYNAMIT_NEXT_BUILD to next week
 DYNAMIT_NEXT_BUILD=$(date -d "next week" +%s)
 DYNAMIT_NEXT_BUILD_COMMENT=$(date -d "next week")
 sed -i "s|^DYNAMIT_NEXT_BUILD=.*|DYNAMIT_NEXT_BUILD=${DYNAMIT_NEXT_BUILD} #${DYNAMIT_NEXT_BUILD_COMMENT}|"\
         /home/{{ ansible_user_id }}/dynamit/.env_dynamit
 
+# Run compose file to start DYNAMIT
 docker compose \
     -f /home/{{ ansible_user_id }}/dynamit/dynamit-run.yaml \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env \
     --env-file /home/{{ ansible_user_id }}/dynamit/.env_dynamit up
+
+# Exit when compose failed
 check_command_fail "[dynamit-start.sh] Fatal Error: Composing dynamit-run failed!"
 exit 1
